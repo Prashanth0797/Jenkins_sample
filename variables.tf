cf68b612038dc893844528e4c90aa1a921d2e6c3
Prashanth0797 <52483402+Prashanth0797@users.noreply.github.com>
2019-07-26 18:35:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "instance_count"{}
-variable "instance_name"{}
-variable "my_region"{}
-variable "my_ami"{}
-variable "my_instance_type"{}
+variable "instance_count"{default = 3}
+variable "instance_name"{default = 'prashanth2-'}
+variable "my_region"{default = 'us-east-1'}
+variable "my_ami"{default = 'ami-0b898040803850657'}
+variable "my_instance_type"{default = 't2.micro'}
